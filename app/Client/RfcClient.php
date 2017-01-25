128d056d78a34bfb990181b229fc4a905a1236ca
Cedric van Putten <byCedric@users.noreply.github.com>
2017-01-25 11:38:14 +0100

    Created a RFC handler. Fixes #50.
    * Created a RFC handler to try and retrieve the link for a RFC id
    
    * Updated the RFC client to perform head requests and on the same URL the IETF uses
    
    * Fixed bad English response

---
+++ app/Client/RfcClient.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Client;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\BadResponseException;
+
+final class RfcClient
+{
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * @param Client $client
+     */
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Try and fetch an RFC url, based on a RFC id.
+     *
+     * @param  string $id
+     * @return string|null
+     */
+    public function getById($id)
+    {
+        try {
+            $url      = "https://tools.ietf.org/html/{$id}";
+            $response = $this->client->head($url, [
+                'verify'          => false,
+                'allow_redirects' => false,
+            ]);
+
+            switch ($response->getStatusCode()) {
+                case 200: return $url;
+                case 302: return $response->getHeaderLine('Location');
+            }
+        } catch (BadResponseException $error) {
+            // fall through
+        }
+
+        return null;
+    }
+}
---
+++ app/Handlers/Utility/RfcHandler.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Handlers\Utility;
+
+use App\Slack\Event;
+use App\Slack\Message;
+use App\Handlers\Handler;
+use App\Client\RfcClient;
+
+final class RfcHandler extends Handler
+{
+    /**
+     * @var RfcClient
+     */
+    private $client;
+
+    /**
+     * @param RfcClient $client
+     */
+    public function __construct(RfcClient $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function canHandle(Event $event)
+    {
+        return
+            $event->isMessage() &&
+            ($event->isDirectMessage() || $event->mentions($this->eve->userId())) &&
+            $event->matches('/\b(rfc)\b/i')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handle(Event $event)
+    {
+        $id       = $this->getRfcId($event->text());
+        $response = 'You have to provide a numeric RFC id, so I can give you the link.';
+
+        if ($id) {
+            $url      = $this->client->getById($id);
+            $response = $url ? $url : "I could not find `RFC {$id}`...";
+        }
+
+        $this->send(
+            Message::saying($response)
+                ->inChannel($event->channel())
+                ->to($event->sender())
+        );
+    }
+
+    /**
+     * Extract the numeric RFC id out of the query string.
+     *
+     * @param  string $query
+     * @return string
+     */
+    private function getRfcId($query)
+    {
+        return filter_var($query, FILTER_SANITIZE_NUMBER_INT);
+    }
+}
